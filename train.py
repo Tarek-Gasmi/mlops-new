a957de11f9f90bdcbfe5178360cd7c63cfe5ddd3
Tarek-Gasmi <77486321+Tarek-Gasmi@users.noreply.github.com>
2021-06-30 20:46:47 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
@@ -13,35 +14,45 @@ seed = 42
 
 # Load in the data
 df = pd.read_csv("wine_quality.csv")
-
+# Create Classification version of target variable
+df['goodquality'] = [1 if x >= 6 else 0 for x in df['quality']]
+# Separate feature variables and target variable
+X = df.drop(['quality','goodquality'], axis = 1)
+y = df['goodquality']
 # Split into train and test sections
-y = df.pop("quality")
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+
 
 #################################
 ########## MODELLING ############
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(max_depth=5, random_state=seed)
-regr.fit(X_train, y_train)
-
+model = RandomForestClassifier(n_estimators=50, max_depth=7, random_state=seed)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
 # Report training set score
-train_score = regr.score(X_train, y_train) * 100
+train_score = model.score(X_train, y_train) * 100
 # Report test set score
-test_score = regr.score(X_test, y_test) * 100
-
+test_score = model.score(X_test, y_test) * 100
+# Report RMSE
+mse = mean_squared_error(y_test, predictions)
+# Report RMSE
+rmse = np.sqrt(mse)
+# Report Classification error report
+error_report = classification_report(y_test, predictions)
 # Write scores to a file
 with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
-
+         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+         outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+         outfile.write("Root Mean Squared Error: %2.1f%%\n" % rmse)
+         outfile.write("Classification Error Report:\n %s\n" %error_report)
 
 ##########################################
 ##### PLOT FEATURE IMPORTANCE ############
 ##########################################
 # Calculate feature importance in random forest
-importances = regr.feature_importances_
+importances = model.feature_importances_
 labels = df.columns
 feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
 feature_df = feature_df.sort_values(by='importance', ascending=False,)
@@ -59,27 +70,3 @@ ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 plt.tight_layout()
 plt.savefig("feature_importance.png",dpi=120) 
 plt.close()
-
-
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
-
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
-
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
-
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
-
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
-
